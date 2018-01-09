773a9208b8467fe8697aa9c31c8fc2dc11384f76
Steven Del Rosario <stevendelro@gmail.com>
2018-01-09 15:03:49 -0800

    revert to original deploy attempt

--- server/server.js
+++ server/server.js
@@ -1,30 +1,22 @@
-require('babel-polyfill');
+require("babel-polyfill");
 const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
 const path = require('path');
+
+
 const app = express();
 
 const public = path.join(__dirname, '..', 'public');
 const dist = path.join(__dirname, '..', 'dist');
 
-app.get('*', (req, res, next) => {
-  if (req.headers["x-forwarded-proto"] === "https") {
-    next();
-  };
-  res.redirect('https://'+req.hostname+req.url);
-});
-
-const sendIndex = (req, res, next) => {
-  res.sendFile(path.join(public, 'index.html'));
-}
-
 app.use(morgan('dev'));
 app.use(express.static(dist));
 app.use(express.static(public));
 
-
-app.all('*', sendIndex)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(public, 'index.html'));
+});
 
 
 
