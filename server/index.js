dd564df4d6221c6e086666299a00e33c8e938d55
Steven Del Rosario <stevendelro@gmail.com>
2018-01-09 14:54:52 -0800

    included https redirect for heroku deploy attempt

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 /* eslint no-console: "off" */
 const server = require('./server');
-
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
--- server/server.js
+++ server/server.js
@@ -1,49 +1,31 @@
-require("babel-polyfill");
-const url = `${ROOT_URL}?s=${searchTerm}${API_KEY}`;
+require('babel-polyfill');
 const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
 const path = require('path');
-
-
 const app = express();
 
 const public = path.join(__dirname, '..', 'public');
 const dist = path.join(__dirname, '..', 'dist');
 
+app.get('*', (req, res, next) => {
+  if (req.headers["x-forwarded-proto"] === "https") {
+    next();
+  };
+  res.redirect('https://'+req.hostname+req.url);
+});
+
+const sendIndex = (req, res, next) => {
+  res.sendFile(path.join(public, 'index.html'));
+}
+
 app.use(morgan('dev'));
 app.use(express.static(dist));
 app.use(express.static(public));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(public, 'index.html'));
-});
 
-app.get('/s=', (req,res) => {
-   axios.get(url)
-    .then(response => {
-      console.log('response:' , response.data);
-      res.send(response.data);
-    })
-    .catch(err => res.send('/s= error:',err.message));
-});
+app.all('*', sendIndex)
 
-app.get('/t=', (req,res) => {
-  axios.get(url)
-   .then(response => {
-     console.log('response:' , response.data);
-     res.send(response.data);
-   })
-   .catch(err => res.send('/t= error:',err.message));
-});
 
-app.get('/i=', (req,res) => {
-  axios.get(url)
-   .then(response => {
-     console.log('response:' , response.data);
-     res.send(response.data);
-   })
-   .catch(err => res.send('/i= error:',err.message));
-});
 
 module.exports = app;
