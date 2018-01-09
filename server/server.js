69ed624c79e84c7c38360863c49b8e0a49342384
Steven Del Rosario <stevendelro@gmail.com>
2018-01-09 15:36:55 -0800

    included CORS functionality

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ const app = express();
 const public = path.join(__dirname, '..', 'public');
 const dist = path.join(__dirname, '..', 'dist');
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+}
+
+app.use(allowCrossDomain);
 app.use(morgan('dev'));
 app.use(express.static(dist));
 app.use(express.static(public));
