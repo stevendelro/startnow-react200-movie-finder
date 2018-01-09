441f72625d240976b41583c59208bd44726fecad
Steven Del Rosario <stevendelro@gmail.com>
2018-01-09 15:47:31 -0800

    prettier formating

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-require("babel-polyfill");
+require('babel-polyfill');
 const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
 const path = require('path');
-
 
 const app = express();
 const public = path.join(__dirname, '..', 'public');
 const dist = path.join(__dirname, '..', 'dist');
 
 const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
-}
+};
 
 app.use(allowCrossDomain);
 app.use(morgan('dev'));
@@ -25,6 +27,5 @@ app.use(express.static(public));
 app.get('*', (req, res) => {
   res.sendFile(path.join(public, 'index.html'));
 });
-
 
 module.exports = app;
